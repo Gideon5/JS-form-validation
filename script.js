0e5884f94302069ae9b559cb53bc84fe6eaea236
elgrande <abaaneg@gmail.com>
2023-01-28 22:03:59 +0000

    object literal

---
+++ script.js
@@ -0,0 +1,11 @@
+let car = {
+    name: "Volvo",
+    color: "Red",
+    transmission: "Automatic",
+    speed: "200km/h",
+    class: "C",
+    startCar: function () {console.log("Car started")},
+    Interior: ["C", "s", "gears", "seats", ["wims", "sprites"," anchors", "testing"]]
+}
+
+console.log(car)
