2bed396c8933a5fa127602ce0454785b02f5a869
elgrande <abaaneg@gmail.com>
2023-02-01 01:14:00 +0000

    php db

--- process.php
+++ process.php
@@ -2,6 +2,11 @@
 
 echo 'Processing';
 
+//Check for a POST variable
+if(isset($_POST['name'])){
+    echo 'POST: Your name is '. $_POST['name'];
+}
+
 
 //Check for a GET variable
 if(isset($_GET['name'])){
